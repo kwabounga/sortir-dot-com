ceeb0cf4d4d611373b5f9744200963320bcf8639
b3col3 <b3col3@gmail.com>
2020-11-02 16:12:37 +0100

    Ajout de la barre de navigation

---
+++ src/Controller/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MainController extends AbstractController {
+
+    /**
+     * @Route("/", name="main")
+     */
+    public function main() {
+        return $this->redirectToRoute('main_home');
+    }
+
+    /**
+     * @Route("/website", name="main_home")
+     */
+    public function home() {
+        return $this->render('main/home.html.twig');
+    }
+}
