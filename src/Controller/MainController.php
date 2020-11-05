3839fe4419c23e7ecc74990359996c9df7753389
b3col3 <b3col3@gmail.com>
2020-11-05 16:05:48 +0100

    Page Home terminer

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,10 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use App\Form\FiltreHomeType;
+use App\Repository\CampusRepository;
+use App\Repository\SortieRepository;
+use DateTime;
+use FiltreHomeDTO;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends AbstractController {
+
+    private $listeSorties;
+    private $filtre;
 
     /**
      * @Route("/", name="home")
@@ -17,7 +32,19 @@ class MainController extends AbstractController {
     /**
      * @Route("/website", name="main_home")
      */
-    public function home() {
-        return $this->render('main/home.html.twig');
+    public function home(SortieRepository $sortieRepo, Request $request) {
+        $user = $this->getUser();
+        $filtre = new FiltreHomeDTO($user);
+
+        $filtreForm = $this->createForm(FiltreHomeType::class, $filtre);
+        $filtreForm->handleRequest($request);
+
+        if ($filtreForm->isSubmitted() && $filtreForm->isValid()) {
+            $filtre = $filtreForm->getData();
+        }
+
+        $listeSorties = $sortieRepo->findSortieFiltre($filtre, $user->getId());
+
+        return $this->render('main/home.html.twig', ['filtreForm' => $filtreForm->createView(), 'listeSorties' => $listeSorties]);
     }
 }
