fdd7b8e4ba6b89095ec6602f2049d7508be23f99
Alexandre <alexandre.fedini@gmail.com>
2020-11-06 13:53:16 +0100

    ajout sortie OK
    - reste créer nouveau lieu
    - reste CSS

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Etat;
 use App\Entity\Sortie;
 use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,7 +14,7 @@ use App\Form\SortieType;
 /**
  * @Route("/website/sortie")
  */
-class SortieController extends AbstractController {
+class SortieController extends CommonController {
 
     /**
      * @Route("/{id}", name="sortie_detail", requirements={"id": "\d+"})
@@ -29,23 +30,35 @@ class SortieController extends AbstractController {
      */
     public function ajouterSortie(EntityManagerInterface $em, Request $request) {
         $sortie = new Sortie();
-        $sortieForm = $this->createForm(SortieType::class,$sortie, [
+        $sortieForm = $this->createForm(SortieType::class, $sortie, [
             'user' => $this->getUser(),
         ]);
         $sortieForm->handleRequest($request);
         if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+            $sortie->setOrganisateur($this->getUser());
+            $sortie->setCampus($this->getUser()->getCampus());
+            
             if ($sortieForm->get('save')->isClicked()) {
-                dump('save');
+                $sortie->setEtat($this->getDoctrine()->getRepository(Etat::class)->find(1));
+                $em->persist($sortie);
+                $em->flush();
             }
             if ($sortieForm->get('publish')->isClicked()) {
-                dump('publish');
+                $sortie->setEtat($this->getDoctrine()->getRepository(Etat::class)->find(2));
+                $sortie->getParticipants()->add($this->getUser());
+                $em->persist($sortie);
+                $em->flush();
             }
+            return $this->redirectToRoute('home');
                     
         } else {
             return $this->render('sortie/ajouter_sortie.html.twig', [
                 'page_name' => 'Création d\'une sortie',
                 'sortie_form' => $sortieForm->createView(),
-                'user' => $this->getUser() ]);
+                'user' => $this->getUser(),
+                'title' => 'Ajout de Sortie',
+                'routes' => $this->getAllRoutes()
+            ]);
         }
         
         
